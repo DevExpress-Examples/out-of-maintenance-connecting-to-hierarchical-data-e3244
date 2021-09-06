ec83c86a3d41e7199e09deb6a1de08b5fadad771
DevExpressExampleBot <fakeEmail>
2021-09-06 19:57:21 +0300

    Source auto update [skip ci]

--- VB/TreeList_DataBinding/Employees.vb
+++ VB/TreeList_DataBinding/Employees.vb
@@ -3,45 +3,149 @@ Imports System.Collections.Generic
 
 Namespace TreeList_DataBinding
 
-    Public Class Employee
-        Public Property ID() As Integer
-        Public Property ParentID() As Integer
-        Public Property Name() As String
-        Public Property Position() As String
-        Public Property Department() As String
-    End Class
+	Public Class Employee
+		Public Property ID() As Integer
+		Public Property ParentID() As Integer
+		Public Property Name() As String
+		Public Property Position() As String
+		Public Property Department() As String
+	End Class
 
-    Public NotInheritable Class Stuff
+	Public Module Stuff
+		Public Function GetStuff() As List(Of Employee)
+'INSTANT VB NOTE: The variable stuff was renamed since it may cause conflicts with calls to static members of the user-defined type with this name:
+			Dim stuff_Conflict As New List(Of Employee)()
+			stuff_Conflict.Add(New Employee() With {
+				.ID = 1,
+				.ParentID = 0,
+				.Name = "Gregory S. Price",
+				.Department = "",
+				.Position = "President"
+			})
+			stuff_Conflict.Add(New Employee() With {
+				.ID = 2,
+				.ParentID = 1,
+				.Name = "Irma R. Marshall",
+				.Department = "Marketing",
+				.Position = "Vice President"
+			})
+			stuff_Conflict.Add(New Employee() With {
+				.ID = 3,
+				.ParentID = 1,
+				.Name = "John C. Powell",
+				.Department = "Operations",
+				.Position = "Vice President"
+			})
+			stuff_Conflict.Add(New Employee() With {
+				.ID = 4,
+				.ParentID = 1,
+				.Name = "Christian P. Laclair",
+				.Department = "Production",
+				.Position = "Vice President"
+			})
+			stuff_Conflict.Add(New Employee() With {
+				.ID = 5,
+				.ParentID = 1,
+				.Name = "Karen J. Kelly",
+				.Department = "Finance",
+				.Position = "Vice President"
+			})
 
-        Private Sub New()
-        End Sub
+			stuff_Conflict.Add(New Employee() With {
+				.ID = 6,
+				.ParentID = 2,
+				.Name = "Brian C. Cowling",
+				.Department = "Marketing",
+				.Position = "Manager"
+			})
+			stuff_Conflict.Add(New Employee() With {
+				.ID = 7,
+				.ParentID = 2,
+				.Name = "Thomas C. Dawson",
+				.Department = "Marketing",
+				.Position = "Manager"
+			})
+			stuff_Conflict.Add(New Employee() With {
+				.ID = 8,
+				.ParentID = 2,
+				.Name = "Angel M. Wilson",
+				.Department = "Marketing",
+				.Position = "Manager"
+			})
+			stuff_Conflict.Add(New Employee() With {
+				.ID = 9,
+				.ParentID = 2,
+				.Name = "Bryan R. Henderson",
+				.Department = "Marketing",
+				.Position = "Manager"
+			})
 
-        Public Shared Function GetStuff() As List(Of Employee)
+			stuff_Conflict.Add(New Employee() With {
+				.ID = 10,
+				.ParentID = 3,
+				.Name = "Harold S. Brandes",
+				.Department = "Operations",
+				.Position = "Manager"
+			})
+			stuff_Conflict.Add(New Employee() With {
+				.ID = 11,
+				.ParentID = 3,
+				.Name = "Michael S. Blevins",
+				.Department = "Operations",
+				.Position = "Manager"
+			})
+			stuff_Conflict.Add(New Employee() With {
+				.ID = 12,
+				.ParentID = 3,
+				.Name = "Jan K. Sisk",
+				.Department = "Operations",
+				.Position = "Manager"
+			})
+			stuff_Conflict.Add(New Employee() With {
+				.ID = 13,
+				.ParentID = 3,
+				.Name = "Sidney L. Holder",
+				.Department = "Operations",
+				.Position = "Manager"
+			})
 
-            Dim stuff_Renamed As New List(Of Employee)()
-            stuff_Renamed.Add(New Employee() With {.ID = 1, .ParentID = 0, .Name = "Gregory S. Price", .Department = "", .Position = "President"})
-            stuff_Renamed.Add(New Employee() With {.ID = 2, .ParentID = 1, .Name = "Irma R. Marshall", .Department = "Marketing", .Position = "Vice President"})
-            stuff_Renamed.Add(New Employee() With {.ID = 3, .ParentID = 1, .Name = "John C. Powell", .Department = "Operations", .Position = "Vice President"})
-            stuff_Renamed.Add(New Employee() With {.ID = 4, .ParentID = 1, .Name = "Christian P. Laclair", .Department = "Production", .Position = "Vice President"})
-            stuff_Renamed.Add(New Employee() With {.ID = 5, .ParentID = 1, .Name = "Karen J. Kelly", .Department = "Finance", .Position = "Vice President"})
+			stuff_Conflict.Add(New Employee() With {
+				.ID = 14,
+				.ParentID = 4,
+				.Name = "James L. Kelsey",
+				.Department = "Production",
+				.Position = "Manager"
+			})
+			stuff_Conflict.Add(New Employee() With {
+				.ID = 15,
+				.ParentID = 4,
+				.Name = "Howard M. Carpenter",
+				.Department = "Production",
+				.Position = "Manager"
+			})
+			stuff_Conflict.Add(New Employee() With {
+				.ID = 16,
+				.ParentID = 4,
+				.Name = "Jennifer T. Tapia",
+				.Department = "Production",
+				.Position = "Manager"
+			})
 
-            stuff_Renamed.Add(New Employee() With {.ID = 6, .ParentID = 2, .Name = "Brian C. Cowling", .Department = "Marketing", .Position = "Manager"})
-            stuff_Renamed.Add(New Employee() With {.ID = 7, .ParentID = 2, .Name = "Thomas C. Dawson", .Department = "Marketing", .Position = "Manager"})
-            stuff_Renamed.Add(New Employee() With {.ID = 8, .ParentID = 2, .Name = "Angel M. Wilson", .Department = "Marketing", .Position = "Manager"})
-            stuff_Renamed.Add(New Employee() With {.ID = 9, .ParentID = 2, .Name = "Bryan R. Henderson", .Department = "Marketing", .Position = "Manager"})
-
-            stuff_Renamed.Add(New Employee() With {.ID = 10, .ParentID = 3, .Name = "Harold S. Brandes", .Department = "Operations", .Position = "Manager"})
-            stuff_Renamed.Add(New Employee() With {.ID = 11, .ParentID = 3, .Name = "Michael S. Blevins", .Department = "Operations", .Position = "Manager"})
-            stuff_Renamed.Add(New Employee() With {.ID = 12, .ParentID = 3, .Name = "Jan K. Sisk", .Department = "Operations", .Position = "Manager"})
-            stuff_Renamed.Add(New Employee() With {.ID = 13, .ParentID = 3, .Name = "Sidney L. Holder", .Department = "Operations", .Position = "Manager"})
-
-            stuff_Renamed.Add(New Employee() With {.ID = 14, .ParentID = 4, .Name = "James L. Kelsey", .Department = "Production", .Position = "Manager"})
-            stuff_Renamed.Add(New Employee() With {.ID = 15, .ParentID = 4, .Name = "Howard M. Carpenter", .Department = "Production", .Position = "Manager"})
-            stuff_Renamed.Add(New Employee() With {.ID = 16, .ParentID = 4, .Name = "Jennifer T. Tapia", .Department = "Production", .Position = "Manager"})
-
-            stuff_Renamed.Add(New Employee() With {.ID = 17, .ParentID = 5, .Name = "Judith P. Underhill", .Department = "Finance", .Position = "Manager"})
-            stuff_Renamed.Add(New Employee() With {.ID = 18, .ParentID = 5, .Name = "Russell E. Belton", .Department = "Finance", .Position = "Manager"})
-            Return stuff_Renamed
-        End Function
-    End Class
+			stuff_Conflict.Add(New Employee() With {
+				.ID = 17,
+				.ParentID = 5,
+				.Name = "Judith P. Underhill",
+				.Department = "Finance",
+				.Position = "Manager"
+			})
+			stuff_Conflict.Add(New Employee() With {
+				.ID = 18,
+				.ParentID = 5,
+				.Name = "Russell E. Belton",
+				.Department = "Finance",
+				.Position = "Manager"
+			})
+			Return stuff_Conflict
+		End Function
+	End Module
 End Namespace
--- VB/TreeList_DataBinding/MainWindow.xaml.vb
+++ VB/TreeList_DataBinding/MainWindow.xaml.vb
@@ -1,16 +1,16 @@
 ﻿Imports System.Windows
 
 Namespace TreeList_DataBinding
-    ''' <summary>
-    ''' Interaction logic for MainWindow.xaml
-    ''' </summary>
-    Partial Public Class MainWindow
-        Inherits Window
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits Window
 
-        Public Sub New()
-            InitializeComponent()
-            treeList.ItemsSource = Stuff.GetStuff()
-            treeListView1.ExpandAllNodes()
-        End Sub
-    End Class
+		Public Sub New()
+			InitializeComponent()
+			treeList.ItemsSource = Stuff.GetStuff()
+			treeListView1.ExpandAllNodes()
+		End Sub
+	End Class
 End Namespace
