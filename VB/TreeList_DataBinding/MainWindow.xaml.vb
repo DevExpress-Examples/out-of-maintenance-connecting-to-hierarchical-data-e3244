f84fd096f6511773a232debefc8efbb7fbdcc139
DevExpressExampleBot <fakeEmail>
2023-04-14 18:53:37 +0400

    Source auto update [skip ci]

--- VB/TreeList_DataBinding/MainWindow.xaml.vb
+++ VB/TreeList_DataBinding/MainWindow.xaml.vb
@@ -2,15 +2,12 @@ Imports System.Windows
 
 Namespace TreeList_DataBinding
 
-    ''' <summary>
-    ''' Interaction logic for MainWindow.xaml
-    ''' </summary>
     Public Partial Class MainWindow
         Inherits Window
 
         Public Sub New()
             Me.InitializeComponent()
-            Me.treeList.ItemsSource = GetStuff()
+            Me.treeList.ItemsSource = GetStaff()
             Me.treeListView1.ExpandAllNodes()
         End Sub
     End Class
